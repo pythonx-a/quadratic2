229de32dbb2121fd4c03eefd32dc57b44f5d96a7
Yves Chevallier <yves.chevallier@heig-vd.ch>
2025-05-06 14:37:32 +0200

    Click + tests

---
+++ quadratic2/__main__.py
@@ -0,0 +1,14 @@
+import click 
+
+from . import quad 
+
+@click.command 
+@click.option('-a', required=True, type=float)
+@click.option('-b', required=True, type=float)
+@click.option('-c', required=True, type=float)
+def main(a, b, c):
+    """ Solve quadratic equation"""
+    x1, x2 = quad(a, b, c)
+    print(f"x1={x1}, x2={x2}")
+
+main()
